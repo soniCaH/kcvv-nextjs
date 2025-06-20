d5747c801cb2a5c66588d1777e5ab2007aaac6bd
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2025-06-20 08:21:31 +0200

    Revert "Move header to hocky."
    This reverts commit 5dac10973e801ed2929a3ac094aae1fbb2a54534.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { PageHeader } from "./components/PageHeader";
-// import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="nl">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="site-wrapper">
-          <PageHeader />
-          {children}
-        </div>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
