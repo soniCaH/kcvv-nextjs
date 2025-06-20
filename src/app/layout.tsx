5dac10973e801ed2929a3ac094aae1fbb2a54534
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2025-06-20 08:19:51 +0200

    Move header to hocky.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "./components/Navbar";
+import { PageHeader } from "./components/PageHeader";
+// import { Navbar } from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="nl">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
+        <div className="site-wrapper">
+          <PageHeader />
+          {children}
+        </div>
       </body>
     </html>
   );
