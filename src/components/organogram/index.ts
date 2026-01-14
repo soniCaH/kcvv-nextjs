840d821258a6330745c48d7605ba8898a27ea1a6
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2026-01-14 08:56:06 +0100

    chore(ui): archive old organogram prototype files (#499)

--- src/components/organogram/index.ts
+++ src/components/organogram/index.ts
@@ -1,4 +1,2 @@
-export { OrgChart } from "./archive/OrgChart";
 export { MemberDetailsModal } from "./MemberDetailsModal";
-export { OrganogramClient } from "./archive/OrganogramClient";
 export { UnifiedOrganogramClient } from "./UnifiedOrganogramClient";
